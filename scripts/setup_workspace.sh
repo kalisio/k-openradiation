3989466608ccfacac7e69ecf2abdba6d24e106a2
cnouguier <ch.nouguier@gmail.com>
2024-04-20 20:37:56 +0200

    wip: Migrate CI to Github actions #7

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -8,47 +8,31 @@ ROOT_DIR=$(dirname "$THIS_DIR")
 
 . "$THIS_DIR/kash/kash.sh"
 
-## Parse options
-##
-
-WORKSPACE_BRANCH=
-WORKSPACE_TAG=
-
-begin_group "Setting up workspace ..."
+begin_group "Setting up openradiation ..."
 
 if [ "$CI" = true ]; then
-    WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
+    openradiation_DIR="$(dirname "$ROOT_DIR")"
     DEVELOPMENT_REPO_URL="https://$GITHUB_DEVELOPMENT_PAT@github.com/kalisio/development.git"
 else
     while getopts "b:t" option; do
         case $option in
             b) # defines branch
-                WORKSPACE_BRANCH=$OPTARG;;
+                openradiation_BRANCH=$OPTARG;;
             t) # defines tag
-                WORKSPACE_TAG=$OPTARG;;
+                openradiation_TAG=$OPTARG;;
             *)
             ;;
         esac
     done
+
     shift $((OPTIND-1))
-    WORKSPACE_DIR="$1"
-
-    # NOTE: cloning krawler could be avoided if we could parse app_version from tag/branch name instead
-    # In this case, the kli would clone krawler
-    GIT_OPS="--recurse-submodules"
-    if [ -n "$WORKSPACE_TAG" ] || [ -n "$WORKSPACE_BRANCH" ]; then
-        GIT_OPS="$GIT_OPS --branch ${WORKSPACE_TAG:-$WORKSPACE_BRANCH}"
-    fi
-    git clone --depth 1 $GIT_OPS "$GITHUB_URL/kalisio/k-openradiation.git" "$WORKSPACE_DIR/k-openradiation"
-
+    openradiation_DIR="$1"
     DEVELOPMENT_REPO_URL="$GITHUB_URL/kalisio/development.git"
 
-    # unset KALISIO_DEVELOPMENT_DIR because we want kli to clone everything in $WORKSPACE_DIR
-    unset KALISIO_DEVELOPMENT_DIR
+    # Clone project in the openradiation
+    git_shallow_clone "$GITHUB_URL/kalisio/k-centipede.git" "$openradiation_DIR/k-centipede" "${openradiation_TAG:-${openradiation_BRANCH:-}}"
 fi
 
-# clone development in $WORKSPACE_DIR
-DEVELOPMENT_DIR="$WORKSPACE_DIR/development"
-git clone --depth 1 "$DEVELOPMENT_REPO_URL" "$DEVELOPMENT_DIR"
+setup_job_openradiation "$openradiation_DIR" "$DEVELOPMENT_REPO_URL"
 
-end_group "Setting up workspace ..."
+end_group "Setting up openradiation ..."
